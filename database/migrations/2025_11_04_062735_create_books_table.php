d4a3b9a491bda78053c896e2024b9c87a6ca9a19
George Nyangaya <george.nyangaya@gmail.com>
2025-11-04 12:45:19 +0300

    Added migrations and created/connected to db

---
+++ database/migrations/2025_11_04_062735_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn', 150);
+            $table->string('title', 150);
+            $table->foreignId('author_id')->constrained('authors')->onDelete('cascade');
+            $table->string('genre', 150);
+            $table->date('published_date');
+            $table->enum('status', ['available', 'borrowed', 'reserved', 'n/a'])->default('available');
+            $table->text('pdf_link',);
+            $table->integer('total_borrow_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
